50183d2e26c6eff2789a428977a936ce5377c428
Fredrik <fredrik@fredrik-Aspire-V3-571.(none)>
2012-10-08 11:22:48 +0200

    trying to change file again

--- uke2/printrandom.py
+++ uke2/printrandom.py
@@ -3,6 +3,7 @@ import random
 a = random.uniform(-1,1) #draw a random number in the interval (-1,1)
 print'random number: %.4f' % (a) # print it to the screen
 
+b = 'I am now trying to change the file and also change it on github'
 '''
 fredrik@fredrik-Aspire-V3-571:~/uio/inf3331/uke2$ python printrandom.py 
 random number: 0.6287
