33ce4df7bf8a9c2da4dd0f1789904785d6c675e4
Fredrik <fredrik@fredrik-Aspire-V3-571.(none)>
2012-10-29 17:00:07 +0100

    nesten ferdig

---
+++ uke9/convert2_wNumPy.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+import sys, math, string
+from numpy import *
+usage = 'Usage: %s infile' % sys.argv[0]
+
+try:
+    infilename = sys.argv[1]
+except:
+    print usage; sys.exit(1)
+
+# load file into a list of lines
+f = open(infilename, 'r'); lines = f.readlines(); f.close()
+
+# the second line contains the increment in t:
+dt = float(lines[1])
+
+# the third line contains the name of the time series:
+ynames = lines[2].split()
+
+# store y data in a dictionary of lists of floats:
+
+y = {}           # declare empty dictionary
+
+# load data from the rest of the lines:
+
+yvalues = loadtxt(infilename,skiprows=3)	#loads the data from file to an array as float. Skips the 3 first rows
+for name in ynames:
+    y[name] = yvalues[:,0]
+f.close()
+print 'y dictionary:\n', y
+
+# write out 2-column files with t and y[name] for each name:
+for name in y.keys():
+    ofile = open(name+'.dat', 'w')
+    for k in range(len(y[name])):
+        ofile.write('%12g %12.5e\n' % (k*dt, y[name][k]))
+    ofile.close()
+'''
+fredrik@fredrik-Aspire-V3-571:~/inf3331/uke9$ python convert2_wNumPy.py mergefile_Sep11_2012.txt 
+y dictionary:
+{'sample_sin.txt': array([ 0.        ,  0.00314316,  0.00628629, ..., -0.00628629,
+       -0.00314316, -0.        ]), 'sample_exp.txt': array([ 0.        ,  0.00314316,  0.00628629, ..., -0.00628629,
+       -0.00314316, -0.        ]), 'sample_cos.txt': array([ 0.        ,  0.00314316,  0.00628629, ..., -0.00628629,
+       -0.00314316, -0.        ])}
+'''
+
---
+++ uke9/gutenberg.py
@@ -0,0 +1,50 @@
+"""
+This program reads the filename to a textfile from the commandline and counts the number of occurances 
+for each word in that file (book). The results are printed alphabetically to a report file called 
+<inputfile>_report.txt.
+"""
+
+import sys
+
+try:
+	infilename = sys.argv[1]
+except:
+	print "Please provide a textfile on the commandline"
+	sys.exit(1)
+	
+infile = open(infilename,'r')
+lines = []
+
+for line in infile:
+	temp = line.split()	
+	for word in temp:	#run through every word in the file
+		nntemp = word.split('--')	#ran into trouble because words were separated by -- and no space
+		if len(nntemp)>1:
+			for i in nntemp:
+				lines.append((i.strip(""".',_;"-:*!?&+()[]""")))	#strip all words of boundary symbols
+		else:
+			lines.append((word.strip(""".,_;"-:*'!?&+()[]""")))		#same
+
+infile.close()
+keywords = {}
+
+for word in lines:
+	if word not in keywords.keys():		# count occurances of every word except garbage
+		if word != '' and word != "'":
+			keywords[word] = 1
+	else:
+		keywords[word] += 1
+		
+infilename = infilename.split('.')[0]
+reportname = infilename +'_report.txt'
+outfile = open(reportname,'w')
+
+for key, value in sorted(dict.items(keywords)):		#print to file alphabetically
+	outfile.write(key),outfile.write('    '+str(value)+'\n')
+
+outfile.close()
+print 'Done! Report is stored in file %s' % reportname
+
+'''
+fredrik@fredrik-Aspire-V3-571:~/inf3331/uke9$ python gutenberg.py blackwood.txt Done! Report is stored in file blackwood_report.txt
+'''
